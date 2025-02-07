8a34653a8ade735e1cd6ee47289ee3d17338e20b
asdzxcvbn1509a <141426904+asdzxcvbn1509a@users.noreply.github.com>
2025-02-07 21:40:11 +0700

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a, b=1;
+    cout << "Enter the number of factorial : ";
+    cin >> a;
+    for (int i=a; i>0; --i) {
+        b = b*i;
+    }
+    cout << "The factorial = " << b;
+    return 0;
+}
