1b66fcb80d559c91a4e4d242b3eb602f8cec648b
asdzxcvbn1509a <141426904+asdzxcvbn1509a@users.noreply.github.com>
2025-02-07 21:39:48 +0700

    Create Matrix_Addition.cpp

---
+++ Matrix_Addition.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Enter the area of matrix : ";
+    cin >> m >> n;
+    cout << endl << "Emter the number of frist matrix" << endl;
+    int first[m][n], second[m][n];
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> first[i][j];
+        }
+    }
+    cout << endl << "Enter the number of second matrix :" << endl;
+    for (int i=0; i<m; i++) {
+        for (int j=0; j<n; j++) {
+            cin >> second[i][j];
+        }
+    }
+    cout << endl << "The addition of both matrix = " << endl;
+    for (int i=0; i<m; i++) {
+        cout << "| ";
+        for (int j=0; j<n; j++) {
+            cout << first[i][j] + second[i][j] << " ";
+        }
+        cout << "|" ;
+        cout << "\n";
+    }
+    return 0;
+}
